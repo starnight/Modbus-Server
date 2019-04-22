677e4a75bd60f9ac8a926c5f43f9538b5c1e969f
Jian-Hong Pan <starnight@g.ncu.edu.tw>
2019-04-22 22:07:28 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# MODBUS Server
+
+This is a MODBUS TCP server simulator.
+
+## Execute
+
+Make sure the Python is version 3.7 or newer.
+
+```sh
+./server.py
+```
+
+The default listening port is **5020**.
+
+## Impelemented Functions
+
+1. Read Coils
+2. Read Discrete Inputs
+3. Read Holding Registers
+4. Read Input Registers
+5. Write Single Coil
+6. Write Single Register
+7. Write Multiple Coils
+8. Write Multiple registers
+
+* Server will response **ILLEGAL FUNCTION** exception if the request function code is not listed above.
+* Server will response **ILLEGAL DATA ADDRESS** exception if the request address is invalid.
+* Server will response **ILLEGAL DATA VALUE** exception if the request data value is invalid.
+* Server will response random values if the reading request format is correct.
+* Server will accept all of the written values if the writing request format is correct.
+
+## Reference:
+
+* [MODBUS Application Protocol Specification V1.1b3](http://www.modbus.org/docs/Modbus_Application_Protocol_V1_1b3.pdf)
+* [MODBUS Messaging on TCP/IP Implementation Guide V1.0b](http://www.modbus.org/docs/Modbus_Messaging_Implementation_Guide_V1_0b.pdf)
---
+++ server.py
@@ -0,0 +1,290 @@
+#!/bin/env python3
+
+from enum import IntEnum
+from math import ceil
+from random import randint
+from time import time
+import asyncio, socket
+
+MODBUS_TCP_MAX_BUF_LEN = 253 + 7
+
+class modbus_exception(IntEnum):
+    ILLEGAL_FUNCTION = 0x1
+    ILLEGAL_DATA_ADDRESS = 0x2
+    ILLEGAL_DATA_VALUE = 0x3
+
+class modbus_request:
+    def __init__(self, buf):
+        self.parse_request(buf)
+
+    def parse_request(self, buf):
+        self.tran_id = int.from_bytes(buf[0:2], byteorder='big')
+        self.proto_id = int.from_bytes(buf[2:4], byteorder='big')
+        self.len_field = int.from_bytes(buf[4:6], byteorder='big')
+        self.unit = int.from_bytes(buf[6:7], byteorder='big')
+        self.fcode = int.from_bytes(buf[7:8], byteorder='big')
+        self.data = buf[8:]
+
+class modbus_response:
+    def __init__(self, req, fcode, data):
+        self.build_response(req, fcode, data)
+
+    def build_response(self, req, fcode, data):
+        self.tran_id = req.tran_id
+        self.proto_id = req.proto_id
+        self.len_field = 2 + len(data)
+        self.unit = req.unit
+        self.fcode = fcode
+        self.data = data
+
+def have_modbus_exception(req, fcode, error):
+    res_fcode = fcode + 0x80
+    res_data = error.to_bytes(length=1, byteorder='big')
+    return res_fcode, res_data
+
+def check_pdu(fcode, data):
+    if not check_fcode(fcode):
+        return modbus_exception.ILLEGAL_FUNCTION
+
+    if len(data) < 2:
+        return modbus_exception.ILLEGAL_DATA_ADDRESS
+
+    if 0x0 < fcode and fcode <= 0x6 and len(data) == 4:
+        return None
+    elif fcode == 0xF and len(data) > 6:
+        return None
+    elif fcode == 0x10 and len(data) > 7:
+        return None
+
+    return modbus_exception.ILLEGAL_DATA_VALUE
+
+def have_addr_cnt(data):
+    addr = int.from_bytes(data[0:2], byteorder='big')
+    cnt = int.from_bytes(data[2:4], byteorder='big')
+    return addr, cnt
+
+def read_coils(fcode, data):
+    addr, cnt = have_addr_cnt(data)
+    if cnt < 0 or cnt > 0x07D0:
+        err = modbus_exception.ILLEGAL_DATA_VALUE
+        return have_modbus_exception(fcode, err)
+
+    if addr > 0xFFFF or addr + cnt - 1 > 0xFFFF:
+        err = modbus_exception.ILLEGAL_DATA_ADDRESS
+        return have_modbus_exception(fcode, err)
+
+    byte_cnt = ceil(cnt / 8)
+    res_data = byte_cnt.to_bytes(length=1, byteorder='big')
+    val = randint(0, pow(2, cnt) - 1)
+    res_data += val.to_bytes(length=byte_cnt, byteorder='big')
+
+    return fcode, res_data
+
+def read_discrete_inputs(fcode, data):
+    return read_coils(fcode, data)
+
+def read_holding_registers(fcode, data):
+    addr, cnt = have_addr_cnt(data)
+    if cnt < 0 or cnt > 125:
+        err = modbus_exception.ILLEGAL_DATA_VALUE
+        return have_modbus_exception(fcode)
+
+    if addr > 0xFFFF or addr + cnt - 1 > 0xFFFF:
+        err = modbus_exception.ILLEGAL_DATA_ADDRESS
+        return have_modbus_exception(fcode, err)
+
+    byte_cnt = cnt * 2
+    res_data = byte_cnt.to_bytes(length=1, byteorder='big')
+    for i in range(cnt):
+        val = randint(0, pow(2, 16) - 1)
+        res_data += val.to_bytes(length=2, byteorder='big')
+
+    return fcode, res_data
+
+def read_input_registers(fcode, data):
+    return read_holding_registers(fcode, data)
+
+def have_addr_val(paylaod):
+    addr = int.from_bytes(data[0:2], byteorder='big')
+    val = int.from_bytes(data[2:4], byteorder='big')
+    return addr, val
+
+def write_single_coil(fcode, data):
+    addr, val = have_addr_val(data)
+    if val != 0x0 and val != 0xFF00:
+        err = modbus_exception.ILLEGAL_DATA_VALUE
+        return have_modbus_exception(fcode, err)
+
+    return fcode, data
+
+def write_single_register(fcode, data):
+    addr, val = have_addr_val(data)
+
+    return fcode, data
+
+def have_addr_cnt_vals(data):
+    addr = int.from_bytes(data[0:2], byteorder='big')
+    cnt = int.from_bytes(data[2:4], byteorder='big')
+    byte_cnt = int.from_bytes(data[4:5], byteorder='big')
+    vals = int.from_bytes(data[5:], byteorder='big')
+    return addr, cnt, byte_cnt, vals
+
+def write_multiple_coils(fcode, data):
+    addr, cnt, byte_cnt, vals = have_addr_cnt_vals(data)
+    if cnt < 0 or cnt > 0x07B0 or \
+       ceil(cnt / 8) != byte_cnt or \
+       byte_cnt != len(vals):
+        err = modbus_exception.ILLEGAL_DATA_VALUE
+        return have_modbus_exception(fcode, err)
+
+    if addr > 0xFFFF or addr + cnt - 1 > 0xFFFF:
+        err = modbus_exception.ILLEGAL_DATA_ADDRESS
+        return have_modbus_exception(fcode, err)
+
+    res_data = addr.to_bytes(length=2, byteorder='big')
+    res_data += cnt.to_bytes(length=2, byteorder='big')
+    return fcode, res_data
+
+def write_multiple_registers(fcode, data):
+    addr, cnt, byte_cnt, vals = have_addr_cnt_vals(data)
+    if cnt < 0 or cnt > 0x7B or \
+       cnt / 2 != byte_cnt or \
+       byte_cnt != len(vals):
+        err = modbus_exception.ILLEGAL_DATA_VALUE
+        return have_modbus_exception(fcode, err)
+
+    if addr > 0xFFFF or addr + cnt - 1 > 0xFFFF:
+        err = modbus_exception.ILLEGAL_DATA_ADDRESS
+        return have_modbus_exception(fcode, err)
+
+    res_data = addr.to_bytes(length=2, byteorder='big')
+    res_data += cnt.to_bytes(length=2, byteorder='big')
+    return fcode, res_data
+
+func_dict = {
+    0x1:  read_coils,
+    0x2:  read_discrete_inputs,
+    0x3:  read_holding_registers,
+    0x4:  read_input_registers, 
+    0x5:  write_single_coil,
+    0x6:  write_single_register,
+    0xf:  write_multiple_coils,
+    0x10: write_multiple_registers,
+}
+
+def check_fcode(fcode):
+    try:
+        func_dict[fcode]
+        res = True
+    except KeyError:
+        res = False
+
+    return res
+
+class modbus_client:
+    def __init__(self, sock, max_elapsed_time=2):
+        self.sock = sock
+        self.max_elapsed_time = max_elapsed_time
+        self.timing_started = False
+
+    def start_timing(self):
+        self.begin = time()
+        self.timing_started = True
+
+    def stop_timing(self):
+        self.timing_started = False
+
+    def is_timeout(self):
+        return self.timing_started and (time() - self.begin > self.max_elapsed_time)
+
+class modbus_server:
+    def __init__(self, ip='0.0.0.0', port=5020, client_num=8, unit=1):
+        self.svr_ip = ip
+        self.svr_port = port
+        self.max_client = client_num
+        self.unit = unit
+        self.svr_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.svr_sock.bind((self.svr_ip, self.svr_port))
+        self.svr_sock.listen(self.max_client)
+        self.svr_sock.setblocking(False)
+
+    def is_this_unit(self, unit):
+        if unit == 0 or unit == self.unit:
+            return True
+        else:
+            return False
+
+    async def send_response(self, client, res):
+        buf = res.tran_id.to_bytes(length=2, byteorder='big')
+        buf += res.proto_id.to_bytes(length=2, byteorder='big')
+        buf += res.len_field.to_bytes(length=2, byteorder='big')
+        buf += res.unit.to_bytes(length=1, byteorder='big')
+        buf += res.fcode.to_bytes(length=1, byteorder='big')
+        buf += res.data
+
+        await self.loop.sock_sendall(client.sock, buf)
+
+    async def handle_transaction(self, client, buf):
+        if len(buf) < 8:
+            client.sock.close()
+            return None
+
+        req = modbus_request(buf)
+
+        print("Client {} transaction id: 0x{:x}, protocol id: 0x{:x}, "
+              "len_field: {}, unit: 0x{:x}, function code: 0x{:x}, "
+              "data length: {} bytes".format(client.sock.getpeername(),
+              req.tran_id, req.proto_id, req.len_field, req.unit, req.fcode,
+              len(req.data)))
+
+        if req.proto_id != 0 or \
+           req.len_field != 1 + 1 + len(req.data) or \
+           not self.is_this_unit(req.unit):
+            client.sock.close();
+            return None
+
+        err = check_pdu(req.fcode, req.data)
+        if err is not None:
+            res_fcode, res_data = have_modbus_exception(req.fcode, err)
+        else:
+            res_fcode, res_data = func_dict[req.fcode](req.fcode, req.data)
+        res = modbus_response(req, res_fcode, res_data)
+
+        await self.send_response(client, res)
+
+    async def handle_client(self, client):
+        buf = await self.loop.sock_recv(client.sock, MODBUS_TCP_MAX_BUF_LEN)
+        if buf is not None and len(buf) > 0:
+            client.stop_timing()
+            await self.handle_transaction(client, buf)
+            client.start_timing()
+        elif client.is_timeout():
+            client.sock.shutdown(socket.SHUT_RDWR)
+            client.sock.close()
+            return None
+
+        self.loop.create_task(self.handle_client(client))
+
+    async def _run_server(self):
+        while True:
+            client_sock, cli_addr = await self.loop.sock_accept(self.svr_sock)
+            print("New client from {}".format(cli_addr))
+            client = modbus_client(client_sock)
+            self.loop.create_task(self.handle_client(client))
+
+    def run(self):
+        self.loop = asyncio.get_event_loop()
+        self.loop.run_until_complete(self._run_server())
+
+    def close(self):
+        self.svr_sock.close()
+        self.loop.close()
+
+    def __del__(self):
+        self.close()
+
+if __name__ == '__main__':
+    server = modbus_server()
+    print("MODBUS server listens on {}:{}".format(server.svr_ip, server.svr_port))
+    server.run()
+    print("MODBUS server closed")
