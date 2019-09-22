4a85e680ea73bf02fff87a9a9b6cca5c35061033
Jian-Hong Pan <starnight@g.ncu.edu.tw>
2019-09-22 22:10:13 +0800

    Split modbus_exception enum into modbus_exception.py

---
+++ modbus_exception.py
@@ -0,0 +1,6 @@
+from enum import IntEnum
+
+class modbus_exception(IntEnum):
+    ILLEGAL_FUNCTION = 0x1
+    ILLEGAL_DATA_ADDRESS = 0x2
+    ILLEGAL_DATA_VALUE = 0x3
--- server.py
+++ server.py
@@ -1,18 +1,14 @@
 #!/bin/env python3
 
-from enum import IntEnum
 from math import ceil
 from random import randint
 from time import time
 import asyncio, socket
 import ssl
 
-MODBUS_TCP_MAX_BUF_LEN = 253 + 7
+from modbus_exception import modbus_exception
 
-class modbus_exception(IntEnum):
-    ILLEGAL_FUNCTION = 0x1
-    ILLEGAL_DATA_ADDRESS = 0x2
-    ILLEGAL_DATA_VALUE = 0x3
+MODBUS_TCP_MAX_BUF_LEN = 253 + 7
 
 class modbus_request:
     def __init__(self, buf):
